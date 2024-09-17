564ec5942e9adf69d9855ed6305ec45195f84704
Rick <ricksmycodeguy@gmail.com>
2024-09-17 11:24:21 -0500

    updates navbar UI and styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,27 @@
-import React from 'react'
+// src/components/NavBar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
 
 function NavBar() {
   return (
-    <div>NavBar</div>
-  )
+    <nav className="navbar">
+      <ul className="nav-links">
+        <li>
+          <Link to="/">Product Recommendations</Link>
+        </li>
+        <li>
+          <Link to="/chatbot">AI Chatbot</Link>
+        </li>
+        <li>
+          <Link to="/image-search">Image Search</Link>
+        </li>
+        <li>
+          <Link to="/sentiment-analysis">Sentiment Analysis</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
