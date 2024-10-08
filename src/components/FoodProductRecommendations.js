4e06a33a5976eaca4d25493f7dfb6857ccfe92d5
Rick <ricksmycodeguy@gmail.com>
2024-10-07 19:30:29 -0500

    chore(api): revert to dynamic url handling due to vercel env var update

--- src/components/FoodProductRecommendations.js
+++ src/components/FoodProductRecommendations.js
@@ -26,7 +26,7 @@ function FoodProductRecommendations() {
     setError(null);
   
     // Perform sentiment analysis first by calling the Express server
-    fetch(apiUrl, {
+    fetch(`${apiUrl}/analyze-sentiment`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
